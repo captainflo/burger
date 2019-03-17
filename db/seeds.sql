35c46df30b8850d6a778b87088f06eb4e2ae3e58
florian lahitte <lahitte.florian@gmail.com>
2019-03-16 22:46:49 -0400

    Add routes function schemas server handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Bacon Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Tomato Chesse', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Pickles Cheese', false);
